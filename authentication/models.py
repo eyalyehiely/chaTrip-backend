b89d4b6f553bbe4e6f8bdf42a1ec1d78fcebfc7b
eyalyehiely <eyalwork0@gmail.com>
2024-10-18 18:51:58 +0900

    rename phone number as user name

--- authentication/models.py
+++ authentication/models.py
@@ -6,12 +6,10 @@ from django.utils import timezone
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone_number = models.CharField(unique=True,max_length=15, blank=True, null=True)
-
-    USERNAME_FIELD = 'phone_number'
+    username = models.CharField(unique=True, max_length=15, blank=True, null=True)
 
     def __str__(self):
-        return self.phone_number
+        return self.username
 
 
 class OTP(models.Model):
@@ -30,4 +28,4 @@ class OTP(models.Model):
         return timezone.now() > self.created_at + timedelta(minutes=5)  # OTP valid for 5 minutes
 
     def __str__(self):
-        return f"OTP for {self.user.phone_number}"
+        return f"OTP for {self.user.username}"
