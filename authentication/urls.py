8eb5350bd3668b1f7cde62c2bd25e53761225fc5
eyalyehiely <eyalwork0@gmail.com>
2024-10-21 02:31:26 +0900

    att token an places api

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,8 +2,16 @@
 
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('send-otp-email/', send_otp_email_view, name='send_otp_email'),
     path('verify-otp-email/', verify_otp_email_view, name='verify_otp_email'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('nearby-places/', get_nearby_places, name='get_nearby_places'),
+
 ]
